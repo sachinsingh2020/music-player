039dfc1b794bd3bad7453a3f9c87e396205b8c3d
sachinsingh2020 <sachinthegreat1001@gmail.com>
2022-10-25 09:00:57 +0530

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-songIndex=0;
-let audioElement= new Audio("./songs/1.mp3")
+let songIndex = 0;
+let audioElement = new Audio("./songs/1.mp3"); ''
 // audioElement.play();
-let masterPlay= document.getElementById('masterPlay')
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gifImage = document.getElementById('gifImage');
+
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gifImage.src = "playing.gif";
+    }
+    else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        gifImage.src = "playing2.png";
+
+    }
+})
+
+audioElement.addEventListener('timeupdate', () => {
+    console.log(parseInt(audioElement.currentTime));
+    progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+    myProgressBar.value = progress;
+});
+
+myProgressBar.addEventListener('change', () => {
+    audioElement.currentTime = (myProgressBar.value * audioElement.duration) / 100;
+})
